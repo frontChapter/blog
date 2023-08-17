2bcbe9b3365b7a70a8b4079a36888f6bc53540c2
Saleh <saleh@welcome.place>
2023-08-17 20:28:13 +0330

    chore: init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/global.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fa">
+      <body>{children}</body>
+    </html>
+  );
+}
