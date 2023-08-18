399ec734151b6f501f7720832fe3a2895d9a4b9a
Saleh <saleh@welcome.place>
2023-08-18 10:18:40 +0330

    chore: trying to fix it

--- src/libs/pocketbase.ts
+++ src/libs/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase'
 
 export const pb = new PocketBase(
-  process.env.BASE_API ?? process.env.NEXT_PUBLIC_BASE_API
+  'https://frontchapter-magic.iran.liara.run'
 ).autoCancellation(false)
